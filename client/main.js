33ebf61340b6a506549a1521e40cbf0fa88e4ca8
juniar_rakhman <juniar.rahman@gmail.com>
2016-04-11 10:22:04 +0700

    almost done

--- client/main.js
+++ client/main.js
@@ -1,7 +1,3 @@
-/**
- * Created by a9jr5626 on 4/4/16.
- */
-
-UI.registerHelper("money", function (amount) {
+UI.registerHelper("money", function(amount) {
     return accounting.formatMoney(amount);
 });
